0377719fb77bddbcf019eab0ce1e0f60f71c77b5
gouerrrr <90602843+gouerrrr@users.noreply.github.com>
2023-09-05 11:38:25 +0800

    do the little rdiff.

--- week2/rdiff/src/main.rs
+++ week2/rdiff/src/main.rs
@@ -1,4 +1,5 @@
 use grid::Grid; // For lcs()
+use std::cmp;
 use std::env;
 use std::fs::File; // For read_file_lines()
 use std::io::{self, BufRead}; // For read_file_lines()
@@ -17,28 +18,68 @@ fn read_file_lines(filename: &String) -> Result<Vec<String>, io::Error> {
         let line_str = line?;
         res.push(line_str);
     }
-    println!("{:?}", res);
+    // println!("{:?}", res);
     Ok(res)
 }
 
-#[allow(unused)] // TODO: delete this line when you implement this function
+// #[allow(unused)] // TODO: delete this line when you implement this function
 fn lcs(seq1: &Vec<String>, seq2: &Vec<String>) -> Grid {
     // Note: Feel free to use unwrap() in this code, as long as you're basically certain it'll
     // never happen. Conceptually, unwrap() is justified here, because there's not really any error
     // condition you're watching out for (i.e. as long as your code is written correctly, nothing
     // external can go wrong that we would want to handle in higher-level functions). The unwrap()
     // calls act like having asserts in C code, i.e. as guards against programming error.
-    unimplemented!();
+    // unimplemented!();
     // Be sure to delete the #[allow(unused)] line above
+    let len_1 = seq1.len();
+    let len_2 = seq2.len();
+    let mut martix = Grid::new(len_1 + 1, len_2 + 1);
+    for i in 0..len_1 + 1 {
+        martix.set(i, 0, 0);
+    }
+    for i in 0..len_2 + 1 {
+        martix.set(0, i, 0);
+    }
+    for i in 0..len_1 {
+        for j in 0..len_2 {
+            if seq1[i] == seq2[j] {
+                martix.set(i + 1, j + 1, martix.get(i, j).unwrap() + 1);
+            } else {
+                martix.set(
+                    i + 1,
+                    j + 1,
+                    cmp::max(martix.get(i, j + 1).unwrap(), martix.get(i + 1, j).unwrap()),
+                );
+            }
+        }
+    }
+    martix
 }
 
-#[allow(unused)] // TODO: delete this line when you implement this function
+// #[allow(unused)] // TODO: delete this line when you implement this function
 fn print_diff(lcs_table: &Grid, lines1: &Vec<String>, lines2: &Vec<String>, i: usize, j: usize) {
-    unimplemented!();
+    // unimplemented!();
     // Be sure to delete the #[allow(unused)] line above
+
+    if i > 0 && j > 0 && lines1[i - 1] == lines2[j - 1] {
+        print_diff(lcs_table, lines1, lines2, i - 1, j - 1);
+        println!("  {}", lines1[i - 1]);
+    } else if j > 0
+        && (i == 0 || lcs_table.get(i, j - 1).unwrap() >= lcs_table.get(i - 1, j).unwrap())
+    {
+        print_diff(lcs_table, lines1, lines2, i, j - 1);
+        println!("> {}", lines2[j - 1]);
+    } else if i > 0
+        && (j == 0 || lcs_table.get(i, j - 1).unwrap() < lcs_table.get(i - 1, j).unwrap())
+    {
+        print_diff(lcs_table, lines1, lines2, i - 1, j);
+        println!("< {}", lines1[i - 1]);
+    } else {
+        println!("  ");
+    }
 }
 
-#[allow(unused)] // TODO: delete this line when you implement this function
+// #[allow(unused)] // TODO: delete this line when you implement this function
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
@@ -48,8 +89,15 @@ fn main() {
     let filename1 = &args[1];
     let filename2 = &args[2];
 
-    unimplemented!();
+    // unimplemented!();
     // Be sure to delete the #[allow(unused)] line above
+    let text1 = read_file_lines(filename1).unwrap();
+    let text2 = read_file_lines(filename2).unwrap();
+    let martix = lcs(&text1, &text2);
+    let len1 = text1.len();
+    let len2 = text2.len();
+    martix.display();
+    print_diff(&martix, &text1, &text2, len1, len2);
 }
 
 #[cfg(test)]
